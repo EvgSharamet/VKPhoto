86c9f56273607650eacfdfec9ae911681e136dc6
Evgeniia <evg.sharamet@yandex.ru>
2022-04-07 16:22:54 +0200

    KeyBoard Extension

--- VKPhoto/VKPhoto/Controller/SignupController.swift
+++ VKPhoto/VKPhoto/Controller/SignupController.swift
@@ -16,3 +16,15 @@ class SignupController: UIViewController {
         view.prepare()
     }
 }
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
